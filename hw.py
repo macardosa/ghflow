7f06ff78d9e17a2b44031ed4cc55ea470311eb5c
Manuel Alejandro Cardosa <manuelcardosa12@gmail.com>
2021-11-14 00:34:57 +0100

    Add greet_manuel function

--- hw.py
+++ hw.py
@@ -5,6 +5,10 @@ def greet(name=""):
 def greet_evelyn():
     greet("Evelyn")
 
+def greet_manuel():
+    greet("Manuel")
+
 if __name__=="__main__":
     greet()
     greet_evelyn()
+    greet_manuel()
