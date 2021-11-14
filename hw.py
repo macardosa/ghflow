a1ca2b52bdfe8f57d0e6bd2f637a7e1314815159
Manuel Alejandro Cardosa <manuelcardosa12@gmail.com>
2021-11-14 01:18:33 +0100

    Announce age

--- hw.py
+++ hw.py
@@ -12,8 +12,12 @@ def inquire_age():
     age = input("Hey!, what's your age? ")
     return age
 
+def say_age(age):
+    print ("You are ", age, " years old")
+
 if __name__=="__main__":
     greet()
     greet_evelyn()
     greet_manuel()
     manuel_age = inquire_age()
+    say_age(manuel_age)
