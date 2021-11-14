62325ec98d5e347f357d41bbcbb1e2a939caa41e
evelyn <evelyngon@gmail.com>
2021-11-14 05:13:18 -0500

    created a age_group function

--- hw.py
+++ hw.py
@@ -2,6 +2,7 @@
 
 def greet(name=""):
     print ("Hello ", name)
+
 def greet_evelyn():
     greet("Evelyn")
 
@@ -9,11 +10,28 @@ def greet_manuel():
     greet("Manuel")
 
 def inquire_age():
-    age = input("Hey!, what's your age? ")
+    age = int (input("Hey!, what's your age? "))
     return age
+
+def age_range():
+    age = inquire_age()
+  
+    if 1 <= age <= 5:
+          return "toodler"
+    elif 6 <= age <= 13:
+          return "kid"
+    elif 14 <= age <= 18:
+          return "teen"
+    elif 19 <= age <= 59:
+          return "adult"
+    else:
+          return "senior adult"
+
 
 if __name__=="__main__":
     greet()
     greet_evelyn()
     greet_manuel()
     manuel_age = inquire_age()
+    age_group = age_range()
+    print ("age_group:", age_group)
